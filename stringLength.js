09213d65a7f538e7423614b73a7db04080c06e3b
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-03-01 17:17:52 +0000

    added reverse string

---
+++ stringLength.js
@@ -0,0 +1,11 @@
+const stringLength = (string) => {
+    const length = string.length;
+    if (!(length < 1 || length > 10)) {
+        return length
+    }
+}
+
+const stringReverse = (string) => {
+    return string.split('').reverse().join('')
+}
+module.exports = stringLength, stringReverse
---
+++ stringLength.test.js
@@ -0,0 +1,15 @@
+const { stringLength, stringReverse } = require('./stringLength')
+
+
+describe('string length', () => {
+    test('return string length', () => {
+        expect(stringLength('Shafiu')).toBe(6)
+    })
+    test('return str < 0 || str > 0', () => {
+        expect(stringLength('Yushawu')).toBe(7)
+    })
+    test('return reverse of a screen', () => {
+        expect(stringReverse('hello')).toMatch('olleh')
+        // expect(stringReverse('1234')).toBe('4321')
+    })
+})
